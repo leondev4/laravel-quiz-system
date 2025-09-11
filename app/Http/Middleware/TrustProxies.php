fc3c07d7e68bc84bd0ac9ef8c88f89520da4717a
Jesus hdez <csc.jesus@gmail.com>
2025-09-10 21:38:44 -0600

    Agregar un solo proxie

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,8 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies= '*';
-    // ['192.168.100.58']
+    //protected $proxies= '*';
+    protected $proxies= ['192.168.100.58'];
     /**
      * The headers that should be used to detect proxies.
      *
