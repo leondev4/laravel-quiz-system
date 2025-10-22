3a65ab423a2bf714002fa3531a705586ed5bcea1
Jesus hdez <csc.jesus@gmail.com>
2025-10-22 10:55:42 -0600

    Agregar nombre completo sin placeholder

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
        <div>
     <x-input-label for="name" :value="'Full Name'" />
     <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-        autofocus autocomplete="name" placeholder="Full Name" />
+        autofocus autocomplete="name"  />
     <x-input-error :messages="$errors->get('name')" class="mt-2" />
 </div>
 
